fe436641c0e6a7ba50f91ac9d5aaf1dbb93a9ce8
Victor Bogdanow Dornelles <victor.bogdanowdornelles@gmail.com>
2025-06-30 17:59:19 -0300

    feat: add Category entity and repository for expense management

---
+++ src/main/java/victorbwd/api_gerenciamento_despesas/domain/category/Category.java
@@ -0,0 +1,26 @@
+package victorbwd.api_gerenciamento_despesas.domain.category;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    private String description;
+
+    private String color; // Hexadecimal color code (e.g., "#FF5733")
+}
