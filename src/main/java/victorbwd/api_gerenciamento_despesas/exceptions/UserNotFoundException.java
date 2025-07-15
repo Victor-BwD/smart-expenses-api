ebead22daa6982bdd9b18e334da05994c4a90113
Victor Bogdanow Dornelles <victor.bogdanowdornelles@gmail.com>
2025-07-15 10:49:09 -0300

    feat: add UserNotFoundException and handler in GlobalExceptionHandler

---
+++ src/main/java/victorbwd/api_gerenciamento_despesas/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package victorbwd.api_gerenciamento_despesas.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
