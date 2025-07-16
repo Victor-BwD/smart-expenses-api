18caf4204d924b7fac8487e3334f942aebacee32
Victor Bogdanow Dornelles <victor.bogdanowdornelles@gmail.com>
2025-07-16 19:11:48 -0300

    feat: add ExpenseFilterDTO and PagedExpenseResponseDTO for expense filtering and pagination

--- src/main/java/victorbwd/api_gerenciamento_despesas/repositories/ExpensesRepository.java
+++ src/main/java/victorbwd/api_gerenciamento_despesas/repositories/ExpensesRepository.java
@@ -1,7 +1,28 @@
 package victorbwd.api_gerenciamento_despesas.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import victorbwd.api_gerenciamento_despesas.domain.expenses.Expenses;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
 public interface ExpensesRepository extends JpaRepository<Expenses, Long> {
+
+    @Query("SELECT e FROM Expenses e WHERE e.user.id = :userId " +
+            "AND (:startDate IS NULL OR e.date >= :startDate) " +
+            "AND (:endDate IS NULL OR e.date <= :endDate) " +
+            "AND (:categoryId IS NULL OR e.category.id = :categoryId) " +
+            "AND (:description IS NULL OR UPPER(e.description) LIKE UPPER(CONCAT('%', :description, '%')))")
+    Page<Expenses> findExpensesWithFilters(
+            @Param("userId") UUID userId,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate,
+            @Param("categoryId") Long categoryId,
+            @Param("description") String description,
+            Pageable pageable
+            );
 }
